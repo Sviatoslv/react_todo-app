db135404daabb4ec23626c73b92742405ff9ae2c
Пользователь <Sviatoslv>
2019-07-17 16:50:13 +0300

    Updates

--- precache-manifest.4190369e742944f3c632727495266ba4.js
+++ precache-manifest.4190369e742944f3c632727495266ba4.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "846a192574418b197737554b48685981",
+    "revision": "9d4ae0deb1e3c01a241201a3259c1ae4",
     "url": "/react_todo-app/index.html"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
+    "revision": "039b096219a4faab11e5",
     "url": "/react_todo-app/static/css/main.a8e92d56.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_todo-app/static/js/2.3310f33a.chunk.js"
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/react_todo-app/static/js/2.164617b5.chunk.js"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
-    "url": "/react_todo-app/static/js/main.b6ea25b8.chunk.js"
+    "revision": "039b096219a4faab11e5",
+    "url": "/react_todo-app/static/js/main.3d93f6aa.chunk.js"
   },
   {
     "revision": "51af3b3c9c5f8ccb1c53",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_todo-app/precache-manifest.36c0f0e73e4ba58e6c71741bdc35771c.js"
+  "/react_todo-app/precache-manifest.4190369e742944f3c632727495266ba4.js"
 );
 
 self.addEventListener('message', (event) => {
